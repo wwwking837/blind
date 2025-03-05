920770b4b8f30be5f7994b8f621155aa10f70b4b
wwwking837 <muhammadzaroonadnan@gmail.com>
2025-03-05 20:20:48 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+# Take input values from user
+x = input("Enter value of x")
+y = input("Enter value of y")
+
+# Swapping 
+temp = x = y
+y = temp
+
+# Displaying Results after swapping
+ print("value of x after swapping", x)
+  print("value of y after swapping", y)
+12
